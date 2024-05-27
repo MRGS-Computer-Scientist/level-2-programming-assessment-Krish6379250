573754a3e54d9d352a392860e45d2aa30b15544f
Krish6379250 <21180@students.mrgs.school.nz>
2024-05-28 09:54:20 +1200

    Title

--- App.py
+++ App.py
@@ -10,18 +10,21 @@ class App():
         self.window.geometry(str(w_width) + "x" + str(w_height))
         self.window.title(app_title)
 
-        self.top_frame = Frame(background='#F9F9F9', width=w_width, height=500)
+        
+
+        self.top_frame = Frame(background='#F9F9F9', width=w_width, height=200)
+        self.top_frame = Label(text="Navigating App") #work on this later
         self.top_frame.pack()
 
-        self.main_frame = Frame(background=bg_color, width=w_width, height=(w_height-700))
+        self.main_frame = Frame(background=bg_color, width=w_width, height=600)
         self.main_frame.pack()
 
-        self.bottom_frame = Frame(background='white', width=w_width, height=200)
+        self.bottom_frame = Frame(background='#F9F9F9', width=w_width, height=200)
         self.bottom_frame.pack(side="bottom")
 
-        self.home_button = Button(self.main_frame, test="Start", height=100, width=200, bg="#7899D4", command=exit)
-        #change command from exit to next page
-        self.home_button.place(x=50, y=100)
+        self.home_button = Button(self.main_frame, text="Start", height=10, width=75, bg="#7899D4", command=exit)
+        #change command from exit to next page later
+        self.home_button.place(x=30, y=400)
 
 
         self.dirname = path.dirname(__file__)
--- app_setting.py
+++ app_setting.py
@@ -1,6 +1,6 @@
-w_width = 500
-w_height = 800
+w_width = 600
+w_height = 1000
 
 app_title = "Navigating App"
 
-bg_color = "#6883BA"
+bg_color = "#F9F9F9"
